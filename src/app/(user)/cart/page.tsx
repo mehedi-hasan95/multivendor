3944e72549c1ca97bfaab21078c9d38c0f479c13
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2025-06-01 21:21:07 +0600

    test 3

--- src/app/(user)/cart/page.tsx
+++ src/app/(user)/cart/page.tsx
@@ -1,5 +1,15 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 const CartPage = async () => {
-  return <div>CartPage</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.cart.getCart.queryOptions());
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>Mehedi</Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default CartPage;
