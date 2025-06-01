b8cd457082512e36847e566f2b01aeda0025568b
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2025-06-01 21:51:53 +0600

    another test

--- src/app/(user)/cart/page.tsx
+++ src/app/(user)/cart/page.tsx
@@ -1,10 +1,14 @@
+import { getQueryClient, trpc } from "@/trpc/server";
 import { CartItems } from "./cart-items";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 const CartPage = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.cart.getCart.queryOptions());
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CartItems />
-    </>
+    </HydrationBoundary>
   );
 };
 
