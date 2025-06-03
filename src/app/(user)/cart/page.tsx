fe36753c3d164c969d16e039e41a58f53168ebf0
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2025-06-03 17:19:04 +0600

    Add some change after install linax

--- src/app/(user)/cart/page.tsx
+++ src/app/(user)/cart/page.tsx
@@ -1,7 +1,19 @@
+export const dynamic = "force-dynamic";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 import { CartItems } from "./cart-items";
 
 const CartPage = async () => {
-  return <CartItems />;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.cart.getCart.queryOptions());
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading cart...</div>}>
+        <CartItems />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default CartPage;
